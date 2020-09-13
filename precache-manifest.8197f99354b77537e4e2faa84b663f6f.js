cf4e82c108e1ffa6b45d4f013d35f360c0451aa1
Akshay Dwivedi <56669557+AkshayKD26@users.noreply.github.com>
2020-09-13 14:32:56 +0530

    Updates

---
+++ precache-manifest.8197f99354b77537e4e2faa84b663f6f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1183cd72b856e3c27a4fb2a3af1c9a24",
+    "url": "/Pacific/index.html"
+  },
+  {
+    "revision": "76608b82747d12553ce4",
+    "url": "/Pacific/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "9300624e0312b5acfae6",
+    "url": "/Pacific/static/css/main.468965d0.chunk.css"
+  },
+  {
+    "revision": "76608b82747d12553ce4",
+    "url": "/Pacific/static/js/2.7ad2da55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pacific/static/js/2.7ad2da55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9300624e0312b5acfae6",
+    "url": "/Pacific/static/js/main.d5f8afe8.chunk.js"
+  },
+  {
+    "revision": "1b1a40a1b98de632e81e",
+    "url": "/Pacific/static/js/runtime-main.b15d29b6.js"
+  }
+]);
